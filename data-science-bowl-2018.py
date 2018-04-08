988f9719f8a836c8d9ca9aa09724ef854c6c514a
Edgar Pavlovsky <edgarpavlovsky@gmail.com>
2018-04-07 18:23:45 -0700

    cleaning up, updating `.html` and `.py`

--- data-science-bowl-2018.py
+++ data-science-bowl-2018.py
@@ -8,7 +8,7 @@
 
 # Import libraries:
 
-# In[232]:
+# In[2]:
 
 import warnings
 import random
@@ -29,14 +29,14 @@ import matplotlib.pyplot as plt
 
 # Configure:
 
-# In[119]:
+# In[3]:
 
 get_ipython().magic('matplotlib inline')
 
 
-# In[91]:
+# In[4]:
 
-warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
+#warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
 seed = 42
 random.seed = seed
 np.random.seed = seed
@@ -44,7 +44,7 @@ np.random.seed = seed
 
 # Set parameters:
 
-# In[127]:
+# In[5]:
 
 # set parameters
 # We can use `IMG_WIDTH` and `IMG_HEIGHT` to downsize the images
@@ -62,26 +62,33 @@ TRAIN_PATH = 'data/stage1_train/'
 TEST_PATH = 'data/stage1_test/'
 
 
-# In[167]:
+# In[6]:
 
 # Get train and test IDs
 train_ids = next(os.walk(TRAIN_PATH))[1]
 test_ids = next(os.walk(TEST_PATH))[1]
 
 
-# In[184]:
+# View a sample image:
+
+# In[7]:
 
 im_id = str(train_ids[45])
 im_full_path = TRAIN_PATH + im_id +'/images/'+ im_id +'.png'
 
 
-# In[185]:
+# In[8]:
 
 im = Image.open(im_full_path)
 arr_im = np.asarray(im)
 
 
-# In[186]:
+# In[9]:
+
+arr_im[:3];
+
+
+# In[12]:
 
 # Print the image dimensions
 print('Original image shape: {}'.format(arr_im.shape))
@@ -91,21 +98,20 @@ print('Original image shape: {}'.format(arr_im.shape))
 # print('New image shape: {}'.format(im_gray.shape))
 
 
-# View a sample image:
-
-# In[157]:
+# In[13]:
 
 im.resize((256,256))
 
 
 # # Get data
 
-# In[230]:
+# In[25]:
 
 def load_data(ids,path,training_data,im_folder='images',              mask_folder='masks',im_file_type='.png',             im_dim_1=520,im_dim_2=696,im_dim_3=4):
     
     # ids: the array of image ids
     # path: the general directory path, e.g. TRAIN_PATH or TEST_PATH
+    # training data: a boolean that specifies whether the data being loaded is training data
     # im_folder: the image subfolder
     # mask_folder: the mask subfolder
     # im_file_type: the image file type. an assumption is made that
@@ -171,14 +177,14 @@ def load_data(ids,path,training_data,im_folder='images',              mask_folde
 
 # Load training data:
 
-# In[231]:
+# In[10]:
 
 X_train, y_train = load_data(ids=train_ids,                             path=TRAIN_PATH,                            training_data=True)
 
 
 # Check if things look good by looking at a random image and its corresponding mask:
 
-# In[234]:
+# In[ ]:
 
 # Check if training data looks all right
 ix = random.randint(0, len(train_ids))
@@ -205,7 +211,7 @@ plt.show()
 # 
 # NOTE: Don't run this stuff, it's not guarenteed to work.
 
-# In[56]:
+# In[ ]:
 
 import pathlib
 import imageio
@@ -213,34 +219,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[64]:
+# In[ ]:
 
 get_ipython().magic('matplotlib inline')
 
 
-# In[5]:
+# In[ ]:
 
 # Glob the training data and load a single image path
 training_paths = pathlib.Path('data/stage1_train/').glob('*/images/*.png')
 training_sorted = sorted([x for x in training_paths])
 
 
-# In[13]:
+# In[ ]:
 
 im_path = training_sorted[45]
 
 
-# In[14]:
+# In[ ]:
 
 im_path
 
 
-# In[15]:
+# In[ ]:
 
 im = imageio.imread(str(im_path))
 
 
-# In[18]:
+# In[ ]:
 
 print('Original image shape: {}'.format(im.shape))
 
@@ -250,7 +256,7 @@ im_gray = rgb2gray(im)
 print('New image shape: {}'.format(im_gray.shape))
 
 
-# In[23]:
+# In[ ]:
 
 # Apply Otsu method to convert images to have bimodal distributions
 
@@ -264,13 +270,13 @@ if np.sum(mask==0) < np.sum(mask==1):
     mask = np.where(mask, 0, 1)
 
 
-# In[24]:
+# In[ ]:
 
 from scipy import ndimage
 labels, nlabels = ndimage.label(mask)
 
 
-# In[28]:
+# In[ ]:
 
 label_arrays = []
 for label_num in range(1, nlabels+1):
@@ -280,7 +286,7 @@ for label_num in range(1, nlabels+1):
 print('There are {} separate components / objects detected.'.format(nlabels))
 
 
-# In[29]:
+# In[ ]:
 
 for label_ind, label_coords in enumerate(ndimage.find_objects(labels)):
     cell = im_gray[label_coords]
@@ -295,7 +301,7 @@ labels, nlabels = ndimage.label(mask)
 print('There are now {} separate components / objects detected.'.format(nlabels))
 
 
-# In[82]:
+# In[ ]:
 
 # Get the object indices, and perform a binary opening procedure
 two_cell_indices = ndimage.find_objects(labels)[1]
@@ -303,12 +309,12 @@ cell_mask = mask[two_cell_indices]
 cell_mask_opened = ndimage.binary_opening(cell_mask, iterations=8)
 
 
-# In[83]:
+# In[ ]:
 
 cell_mask_opened
 
 
-# In[84]:
+# In[ ]:
 
 imgplot = plt.imshow(cell_mask_opened)
 
