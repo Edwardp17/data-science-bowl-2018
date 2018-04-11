0da92cf61660533607a238ceb027688c77dd328e
Edgar Pavlovsky <edgarpavlovsky@gmail.com>
2018-04-10 23:09:41 -0700

    updating main.py

--- src/main.py
+++ src/main.py
@@ -10,34 +10,42 @@ import nn.unet_nb as unet
 from encoding.encoding import RunLengthEncoding
 
 
-def main():
+def main(predict=False):
     # Hyperparameters
+    print("Setting hyperparameters..")
     im_height=512
     im_width=512
-    im_channels=4
+    im_channels=2 # setting im_channels to 2 since masks are black and white
+    print("im_height: "+str(im_height))
+    print("im_width: "+str(im_width))
+    print("im_channels: "+str(im_channels))
     # input_img_resize = (512, 512)  # The resize size of the input images of the neural net
     #output_img_resize = (388, 388)  # The resize size of the output images of the neural net
     #batch_size = 3
-    epochs = 50
+    epochs = 10
+    print("epochs: "+str(epochs))
     thresholds = np.arange(0.5,1,0.05)
     validation_size = 0.2
     # NOTE: Removing sample_size for now to descope, can be implemented later.
     # sample_size = None  # Put 'None' to work on full dataset or a value between 0 and 1
 
     # -- Optional parameters
-    threads = cpu_count()
+    # threads = cpu_count()
     use_cuda = torch.cuda.is_available()
+    print("cuda: "+str(use_cuda))
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Download the datasets
+    print("Downloading dataset if necessary..")
     ds_fetcher = DatasetFetcher()
     ds_fetcher.download_dataset()
 
     # Get training and validation data.
     # NOTE: All of this data is returned as tensors, not numpy arrays
     # as was originally the case in Ekami's implementation.
-    X_train, y_train, X_valid, y_valid = ds_fetcher.get_train_files(validation_size=validation_size)
-    
+    print("Getting training files..")
+    X_train, y_train, X_valid, y_valid = ds_fetcher.get_train_files(validation_size=validation_size,im_dim_3=im_channels)
+    print("Training files loaded!")
     # TODO: Implement get_test_files
     # full_x_test = ds_fetcher.get_test_files(sample_size)
 
@@ -63,40 +71,45 @@ def main():
 
     # Define our neural net architecture.
     in_shape = (im_height, im_width, im_channels)
+    print("Initializing model..")
     net = unet.UNetOriginal(in_shape)
-
+    print("Initializing classifier..")
     classifier = nn.classifier.DSBowlCLassifier(net,epochs)
 
     # because X_train and y_train are already tensors from get_train_files,
     # we don't need to transform them anymore. We just need to put them
     # together in the same tuple and then input them into a DataLoader.
+    print("Loading training data..")
     train_ds = (X_train, y_train)
     train_loader = DataLoader(train_ds,pin_memory=use_cuda)
 
     # same with validation set as with training set
+    print("Loading validation data..")
     valid_ds = (X_valid, y_valid)
     valid_loader = DataLoader(valid_ds,pin_memory=use_cuda)
 
-    print("Training on {} samples and validating on {} samples "
+    print("Training on {} samples and validating on {} samples.."
           .format(len(train_loader.dataset), len(valid_loader.dataset)))
 
     classifier.train(train_loader, valid_loader, epochs)
 
-    # TODO: Implement loading the test dataset
-    # test_ds = (X_test)
-    # test_loader = DataLoader(test_ds,pin_memory=use_cuda)
+    # TODO: The prediction section is still not fully complete
+    if predict == True:
+        print("Loading the test dataset..")
+        test_ds = ds_fetcher.get_test_files(im_dim_3=im_channels)
+        test_loader = DataLoader(test_ds,pin_memory=use_cuda)
 
-    # Predict and save
-    # returns a dict of file names and numpy array prediction masks
-    files_to_pred_masks = classifier.predict(test_loader)
+        # Predict and save
+        # returns a dict of file names and numpy array prediction masks
+        files_to_pred_masks = classifier.predict(test_loader)
 
-    # TODO: We may need to turn predictions from classifications here,
-    # but I don't full understand how that factors in.
-    # I'm looking at the "Check RLE" section of this notebook, for
-    # reference: https://www.kaggle.com/kmader/nuclei-overview-to-submission    
-    encoder = RunLengthEncoding()
-    # this returns a pandas dataframe
-    df_predictions = encoder.encode_predictions(files_to_pred_masks)
-    # output predictions to CSV. Note that the index here is required
-    # because that is where the file names show up.
-    df_predictions.to_csv('predictions.csv',index=True)
+        # TODO: We may need to turn predictions from classifications here,
+        # but I don't full understand how that factors in.
+        # I'm looking at the "Check RLE" section of this notebook, for
+        # reference: https://www.kaggle.com/kmader/nuclei-overview-to-submission    
+        encoder = RunLengthEncoding()
+        # this returns a pandas dataframe
+        df_predictions = encoder.encode_predictions(files_to_pred_masks)
+        # output predictions to CSV. Note that the index here is required
+        # because that is where the file names show up.
+        df_predictions.to_csv('predictions.csv',index=True)
